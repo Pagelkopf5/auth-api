b697cb028fb8db6894da5e56f5a3bd3fa6c93da4
Pagel <gabriel.pagelkopf@gmail.com>
2023-12-26 17:09:50 -0300

    setup elysia

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import {Elysia} from 'elysia';
+
+const app = new Elysia();
+
+app.get('/', () => {
+    return new Response('Hello World!');
+})
+
+app.listen(process.env.PORT || 3000);
