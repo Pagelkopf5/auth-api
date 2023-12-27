7803ab7b4bcebf3ae19bea79ef836e9e0cf30bdf
Pagel <gabriel.pagelkopf@gmail.com>
2023-12-27 18:09:17 -0300

    setup routes

--- index.js
+++ index.js
@@ -1,13 +1,55 @@
 import {Elysia, t} from 'elysia';
+import { swagger } from '@elysiajs/swagger';
+import { cors } from '@elysiajs/cors';
 
-new Elysia()
+const app = new Elysia()
+
+const docsSetup = {
+    path: '/docs',
+    documentation: {
+        info: {
+            title: 'Auth API',
+            version: '1.0.0',
+            description: 'API to handle autentication and authorization',
+        },
+    }
+}
+
+app
+    .use(cors())
+    .use(swagger(docsSetup))
     .get('/', () => 'Hello There!')
     .group('/user', (app) => 
         app
-            .get('/get', () => ('User get'))
-            .get('/find', () => ('User find'))
-            .post('/create', () => ('User create'))
-            .put('/update', () => ('User update'))
-            .delete('/delete', () => ('User delete'))
-    )
+            .get('/auth', () => {
+                return new Response("user auth", {
+                        status: 201,
+                        headers: { 'X-Authorization': "accessToken" }
+                    });
+            })
+            .guard(
+                {
+                    beforeHandle({ cookie: { session } }) {
+                        if (session.value)
+                            return (set.status = 'Unauthorized')
+                    }
+                },
+                (app) =>
+                    app
+                        .get('/get', () => "user get")
+                        .get('/find', () => ('User find'))
+                        .put('/update', () => ('User update'))
+                        .get('/refreshToken', () => ('User refresh token'))
+                        .delete('/delete', () => ('User delete'))
+                        .guard({
+                            body: t.Object({
+                                username: t.String(),
+                                email: t.String(),
+                                password: t.String(),
+                            })
+                        }, (app) =>   
+                            app.post('/create', () => ('User create'))
+                        )
+            )
+        )
     .listen(process.env.PORT || 3000)
